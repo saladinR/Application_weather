7ec3de95f407e2e4e305ae5145f6b6176dd68047
SaladinR <salaheddine.raiss@gmail.com>
2021-12-22 14:34:15 +0100

    final  version

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
-const Apikey = 'aaa46d22f019ca7a19dad7294167b3ec';
+const Apikey = '';
 
-let url=`https://api.openweathermap.org/data/2.5/weather?q=paris&appid=${Apikey}&units=metric&lang=FR`;
+let apiCall = function(city){
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Apikey}&units=metric&lang=FR`;
 
 fetch(url).then( (Response) => 
 Response.json().then((data) => {
     console.log(data);
+    temp_wir = data.main.temp;
+    console.log(temp_wir);
     document.querySelector('#city').innerHTML = "<i class='fas fa-city'></i>" + data.name;
     document.querySelector('#temp').innerHTML = "<i class='fas fa-thermometer-three-quarters'></i>" + data.main.temp + '°';
     document.querySelector('#wind').innerHTML ="<i class='fas fa-wind'></i>" + data.wind.speed + 'km/h';
     document.querySelector('#humidity').innerHTML = "<i class='fas fa-tint'></i>" + data.main.humidity + '%';
+
+   
+   
+
 })
-);
+).catch(err => console.log('erreur :' + err));
+}
+
+    document.querySelector('form').addEventListener('submit', function(e){
+    e.preventDefault();
+    let ville = document.querySelector('#inputCity').value;
+   
+        apiCall(ville);
+    
+
+})
 
